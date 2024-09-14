1d6fd1d46e32422b66b559e54527e4fc5213635d
xxiuminn <xiumin1997@gmail.com>
2024-09-14 23:37:12 +0800

    set up base

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,35 @@
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
+
+const Links = () => {
+  const links = [
+    {
+      title: "Homepage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+
+  return (
+    <div className={styles.links}>
+      {links.map((link) => (
+        <NavLink item={link} key={link.title}>
+          {link.title}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default Links;
