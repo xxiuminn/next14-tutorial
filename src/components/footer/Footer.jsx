87e732b2e89754b38a2e4f9517b7317e7065b1da
xxiuminn <xiumin1997@gmail.com>
2024-09-15 22:37:15 +0800

    update pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,13 @@
+import styles from "./footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}>
+        Lama creative thoughts agency. All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
